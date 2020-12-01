18239151e1091f36d4bc7ed27397f347c343799c
Adrian Hill <adrian.hill@mailbox.org>
2020-12-01 18:33:17 +0100

    Add test using LQR policy

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,6 @@ using DiffEqEnvironments
 using SafeTestsets
 
 @safetestset "DiffEqEnvironments.jl" begin
-    #@safetestset "Random policy" begin include("random_policy.jl") end
     @safetestset "Scalar ODE" begin include("scalar_autonomous_ode.jl") end
+    @safetestset "LQR control" begin include("lqr_control.jl") end
 end
