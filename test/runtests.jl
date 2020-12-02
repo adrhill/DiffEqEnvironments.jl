36c0bdfb60db5cf8c08f3430b58611cc0f31c0c8
Adrian Hill <adrian.hill@mailbox.org>
2020-12-02 16:03:57 +0100

    Time tests, add test for LTIQuadraticEnv

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using DiffEqEnvironments
+using Test
 using SafeTestsets
 
-@safetestset "DiffEqEnvironments.jl" begin
-    @safetestset "Scalar ODE" begin include("scalar_autonomous_ode.jl") end
-    @safetestset "LQR control" begin include("lqr_control.jl") end
+@time @safetestset "DiffEqEnvironments.jl" begin
+    @time @safetestset "Scalar ODE" begin include("scalar_autonomous_ode.jl") end
+    @time @safetestset "LQR control" begin include("lqr_control.jl") end
+    @time @safetestset "LTIQuadraticEnv" begin include("lti_constructor.jl") end
 end
