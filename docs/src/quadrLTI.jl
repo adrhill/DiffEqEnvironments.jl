6692f0c43c4dc36bea2c43ed89e22ecdaf1917fc
Adrian Hill <adrian.hill@mailbox.org>
2021-01-11 13:30:32 +0100

    Fix state-space matrices in quadrLTI test

--- docs/src/quadrLTI.jl
+++ docs/src/quadrLTI.jl
@@ -1,11 +1,8 @@
-using DiffEqEnvironments
 using DifferentialEquations
 using LinearAlgebra
 using ControlSystems
-using Test
-using ReinforcementLearningBase
-using Plots
 
+using Plots
 include("plots.jl")
 
 """
@@ -16,7 +13,7 @@ This is also known as the rocket car problem, where
     s = [p, ṗ]  ,
 p being the position of the car.
 
-The action used as an input to the system is 
+The action used as an input to the system is
 the car's acceleration p̈ ∈ [-1, 1]. Therefore
     ṡ = [ṗ, p̈] = [0 1; 0 0] * s + [0; 1] * u .
 Full observation of the state is given, as
@@ -28,19 +25,23 @@ T = Float32
 A = [0 1; 0 0]
 B = [0; 1]
 C = [1 0; 0 1]
-D = [0][:,:]
+D = [0; 0]
 
 # weights for quadratic cost function
 Q = [1 0; 0 1] * 10
-R = [1][:,:]
+R = [1][:, :]
 
 # Set range of control input
-a_lb = -1.0; a_ub = 1.0
+a_lb = -1.0;
+a_ub = 1.0;
 
 # Define ODEProblem of SISO LTI system
-function lti_ode(s, a, t) 
-    ṡ = A * s + B * a
+function lti_ode(s, a, t)
+    return ṡ = A * s + B * a
 end
+
+s0 = [-0.5f0, -0.5f0]
+tspan = (0.0f0, 5.0f0)
 prob_lti = ODEProblem(lti_ode, s0, tspan)
 
 # Use ControlSystems.jl to solve continuous algebraic Riccati eq.
@@ -51,19 +52,19 @@ println("K=", K)
 v_lqr(s) = -s' * P * s / 2
 π_lqr(s) = clamp(first(K * s), a_lb, a_ub) #  LQR controller w/ saturation
 
-# Define closed-loop ODE for system with input saturations
+# Define closed-loop ODE
 function cl_ode(π)
     return (s, _, t) -> lti_ode(s, π(s), t)
 end
 
-p1 = plot_value(v_lqr)
-p2 = plot_phase_portrait(cl_ode(π_lqr))
-p3 = plot_trajectory(cl_ode(π_lqr), s0)
+p1 = plot_phase_portrait(cl_ode(π_lqr))
+p2 = plot_trajectory(cl_ode(π_lqr), s0)
+p3 = plot_value(v_lqr)
 
-display(plot(p1, p2, p3))
+display(plot(p1, p2, p3; layout=(1, 3), size=(900, 250)))
 
-# Call constructor for LTI systems
-s0 = [0f0, 0f0]
-tspan = (0f0, 5f0)
+# Call constructor for LTI systems to try out LQR Policy
+using DiffEqEnvironments
+using ReinforcementLearningCore
 dt = 0.1
-env = LTIQuadraticEnv(A, B, C, D, Q, R, s0, tspan, dt; a_lb=-1, a_ub=1)
+env = LTIQuadraticEnv(A, B, C, D, Q, R, s0, tspan, dt; a_lb=-1, a_ub=1, T=T)
